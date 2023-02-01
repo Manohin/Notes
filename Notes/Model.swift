39453428498e49402fa33e8ff851fd409769fba4
Manohin <111537419+Manohin@users.noreply.github.com>
2023-02-02 01:50:03 +0300

    Add save data functional with UserDefaults

--- Notes/Model.swift
+++ Notes/Model.swift
@@ -7,4 +7,25 @@
 
 import Foundation
 
-var dataItems = ["Пресс качат", "Турник", "Бегит", "Онжуманя"]
+var dataItems:[String] {
+    set {
+        UserDefaults.standard.set(newValue, forKey: "dataKey")
+        UserDefaults.standard.synchronize()
+    }
+    get {
+        if let array = UserDefaults.standard.array(forKey: "dataKey") as? [String] {
+            return array
+        } else {
+            return[]
+        }
+    }
+}
+
+func addItem(nameItem: String) {
+    dataItems.append(nameItem)
+}
+
+func removeItem(at index: Int) {
+    dataItems.remove(at: index)
+}
+
